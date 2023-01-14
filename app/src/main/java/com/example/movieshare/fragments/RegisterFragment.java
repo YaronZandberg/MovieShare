02aa2e2a017ccc83630542e9c148507110bc38b7
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2023-01-14 23:43:47 +0200

    Done: 1. Create two fragments: LoginFragment & RegisterFragment. 2. Create an action from LoginFragment to RegisterFragment by clicking on register button. 3. Create a nav graph with a nav host in order to define the fragment flow.

---
+++ app/src/main/java/com/example/movieshare/fragments/RegisterFragment.java
@@ -0,0 +1,25 @@
+package com.example.movieshare.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.movieshare.databinding.FragmentRegisterBinding;
+
+public class RegisterFragment extends Fragment {
+    private FragmentRegisterBinding viewBindings;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        this.viewBindings = FragmentRegisterBinding.inflate(inflater, container, false);
+        this.viewBindings.registerFragmentRegisterBtn.setOnClickListener(view ->
+                Toast.makeText(getContext(),"Register button has been clicked",Toast.LENGTH_LONG)
+                        .show());
+        return this.viewBindings.getRoot();
+    }
+}
