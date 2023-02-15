9ca5245b217d1f9783a1d88e41c78f92d501ff1a
Toharush <52196970+toharush@users.noreply.github.com>
2023-02-15 20:36:55 +0200

    More Fixes

--- app/src/main/java/com/example/movieshare/fragments/base/MovieBaseFragment.java
+++ app/src/main/java/com/example/movieshare/fragments/base/MovieBaseFragment.java
@@ -41,7 +41,7 @@ public abstract class MovieBaseFragment extends Fragment {
                 } else {
                     if (Objects.nonNull(view)) {
                         if (menuItem.getItemId() == R.id.logoutMenuItem) {
-                            Repository.getRepositoryInstance().getAuthModel().logout(() -> startGuestActivity());
+                            Repository.getRepositoryInstance().getAuthModel().logout(() -> startIntroActivity());
                         } else {
                             NavDirections action = MovieHomeFragmentDirections.actionGlobalUserProfileFragment();
                             Navigation.findNavController(view).navigate(action);
@@ -54,7 +54,7 @@ public abstract class MovieBaseFragment extends Fragment {
         }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
     }
 
-    protected void startGuestActivity() {
+    protected void startIntroActivity() {
         if (Objects.nonNull(getActivity())) {
             Intent introActivityIntent = new Intent(getActivity(), GuestsActivity.class);
             startActivity(introActivityIntent);
